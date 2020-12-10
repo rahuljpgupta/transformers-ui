c40c42b1c0280427962f9b3304c4b61d39bbff1b
Rahul Gupta <rahuljpgupta@gmail.com>
2020-12-10 14:22:18 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+import NavigationBar from './components/NavigationBar';
+import ComposedChartView from './charts/ComposedChartView';
+import AreaChartView from './charts/AreaChartView';
+import BarChartView from './charts/BarChartView';
+import LineChartView from './charts/LineChartView';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+      <Container>
+        <Row>
+          <Col>
+            <ComposedChartView />
+            <AreaChartView />
+            <BarChartView />
+            <LineChartView />
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
---
+++ src/components/ChartDropdown.jsx
@@ -0,0 +1,27 @@
+import {
+    Navbar,
+    Button,
+    Nav,
+    Form,
+    FormControl
+} from 'react-bootstrap';
+  
+  function ChartDropdown() {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Transformer</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+    );
+  }
+  
+  export default ChartDropdown;
+  
